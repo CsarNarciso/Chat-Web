7134b9c927691b5ffa8b0c0301bd2cdfdff86310
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2024-08-27 15:44:37 -0600

    Initializing project

---
+++ ConfigServer/src/test/java/com/cesar/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
