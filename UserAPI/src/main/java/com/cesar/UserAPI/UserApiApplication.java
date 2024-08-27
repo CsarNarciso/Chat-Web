564a500dd1fab94f85f8a1f14516ee5f87ce65d6
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2024-08-27 16:18:25 -0600

    Initialzing as maven project

---
+++ UserAPI/src/main/java/com/cesar/UserAPI/UserApiApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.UserAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApiApplication.class, args);
+	}
+
+}
---
+++ UserAPI/src/test/java/com/cesar/UserAPI/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.UserAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
