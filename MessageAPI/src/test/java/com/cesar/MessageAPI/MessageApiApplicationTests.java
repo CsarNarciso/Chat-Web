4db1207915b2be38e8babfdd547fcca1f802bc31
CsarNarciso <cesarpazol1029@gmail.com>
2024-08-29 17:41:45 -0600

    Initializing MessageAPI main config

---
+++ MessageAPI/src/test/java/com/cesar/MessageAPI/MessageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.MessageAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
