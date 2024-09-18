dc477d9ec59c9887075c5d4a24cee7289af64e0b
CsarNarciso <cesarpazol1029@gmail.com>
2024-09-18 10:04:30 -0600

    ParticipantAPI start configuration

---
+++ ParticipantAPI/src/test/java/com/cesar/ParticipantAPI/ParticipantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.ParticipantAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParticipantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
