b2054a617f3b3977284959e1de10d8f00ed50980
CsarNarciso <cesarpazol1029@gmail.com>
2024-10-01 11:56:09 -0600

    EurekaServer rename to Eureka

--- Eureka/src/main/java/com/cesar/Eureka/EurekaApplication.java
+++ Eureka/src/main/java/com/cesar/Eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.cesar.EurekaServer;
+package com.cesar.Eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
-
+public class EurekaApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
-
 }
