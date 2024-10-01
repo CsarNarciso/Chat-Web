64a8cce6defcae8076dd98d4374e26775d9e990e
CsarNarciso <cesarpazol1029@gmail.com>
2024-10-01 12:02:06 -0600

    ConfigServer renamed to Config, Configurations folder inside Config

--- Config/src/main/java/com/cesar/Config/ConfigApplication.java
+++ Config/src/main/java/com/cesar/Config/ConfigApplication.java
@@ -1,4 +1,4 @@
-package com.cesar.ConfigServer;
+package com.cesar.Config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
-
+public class ConfigApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(ConfigApplication.class, args);
 	}
-
 }
--- Config/src/test/java/com/cesar/Config/ConfigApplicationTests.java
+++ Config/src/test/java/com/cesar/Config/ConfigApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cesar.ConfigServer;
+package com.cesar.Config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class ConfigApplicationTests {
 
 	@Test
 	void contextLoads() {
