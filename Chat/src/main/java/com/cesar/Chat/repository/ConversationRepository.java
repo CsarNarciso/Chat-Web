475bbcd0c204bc35c6fd79770a748327826a1ab9
CsarNarciso <cesarpazol1029@gmail.com>
2024-10-08 16:43:09 -0600

    Chat sendMessage Service method

--- Chat/src/main/java/com/cesar/Chat/repository/ConversationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cesar.Chat.repository;
-
-import com.cesar.Chat.entity.Conversation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ConversationRepository extends JpaRepository<Conversation, Long> {
-    Conversation findByParticipantsIds(List<Long> participantsIds);
-}
