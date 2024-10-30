14d2db316dc80780c8c4b3a3adc6567e1bb04b0c
CsarNarciso <cesarpazol1029@gmail.com>
2024-10-30 15:21:19 -0600

    User @Repository and @ComponentScan use for repository class

--- User/src/main/java/com/cesar/User/UserApplication.java
+++ User/src/main/java/com/cesar/User/UserApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients
+@ComponentScan("com.cesar.repository")
 @SpringBootApplication
 public class UserApplication {
 	public static void main(String[] args) {
