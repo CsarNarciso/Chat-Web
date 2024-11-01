b7549b5585d9c682e1c897e4e842a31523325116
CsarNarciso <cesarpazol1029@gmail.com>
2024-11-01 16:38:55 -0600

    Social Add Repository annotation

--- Social/src/main/java/com/cesar/Social/repository/NetworkRepository.java
+++ Social/src/main/java/com/cesar/Social/repository/NetworkRepository.java
@@ -2,10 +2,12 @@ package com.cesar.Social.repository;
 
 import com.cesar.Social.entity.Network;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
 
+@Repository
 public interface NetworkRepository extends JpaRepository<Network, Long> {
 
     Network findByUserId(Long userId);
