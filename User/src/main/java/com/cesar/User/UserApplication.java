cdc17c3059f69cc80bcaf97eac38a9bbacf43972
CsarNarciso <cesarpazol1029@gmail.com>
2024-10-31 18:49:44 -0600

    User Starter Jpa dependency instead Data Jpa, wich was causing problem with repository scan on startup

--- User/src/main/java/com/cesar/User/UserApplication.java
+++ User/src/main/java/com/cesar/User/UserApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients
-@ComponentScan("com.cesar.repository")
 @SpringBootApplication
 public class UserApplication {
 	public static void main(String[] args) {
