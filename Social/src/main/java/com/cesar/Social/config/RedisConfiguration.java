b8de9478efedd85920cdae8a079ec825fc72374a
CsarNarciso <cesarpazol1029@gmail.com>
2024-11-01 16:38:00 -0600

    Social exclude Lettuce from Redis dependency

--- Social/src/main/java/com/cesar/Social/config/RedisConfiguration.java
+++ Social/src/main/java/com/cesar/Social/config/RedisConfiguration.java
@@ -6,23 +6,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@EnableRedisRepositories
 @Configuration
 public class RedisConfiguration {
 
-    @Value("redis.hostname")
+    @Value("${redis.hostname}")
     private String REDIS_HOSTNAME;
-    @Value("redis.port")
-    private int REDIS_PORT;
+    @Value("${redis.port}")
+    private Integer REDIS_PORT;
 
     @Bean
     public JedisConnectionFactory connectionFactory(){
-        return new JedisConnectionFactory(
-                new RedisStandaloneConfiguration(REDIS_HOSTNAME, REDIS_PORT));
+        RedisStandaloneConfiguration redisStandaloneConfiguration =
+                new RedisStandaloneConfiguration(REDIS_HOSTNAME, REDIS_PORT);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
