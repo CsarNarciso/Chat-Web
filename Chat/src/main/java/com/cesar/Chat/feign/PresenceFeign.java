1ed1e17ff7e76f6ede605c1a4930a7c2177575be
CsarNarciso <cesarpazol1029@gmail.com>
2024-11-02 16:22:56 -0600

    Chat implementing circuit breaker for user feign client

--- Chat/src/main/java/com/cesar/Chat/feign/PresenceFeign.java
+++ Chat/src/main/java/com/cesar/Chat/feign/PresenceFeign.java
@@ -2,12 +2,29 @@ package com.cesar.Chat.feign;
 
 import com.cesar.Chat.dto.UserPresenceDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import java.util.Collections;
 import java.util.List;
 
-@FeignClient(name="${services.presence.name}", url="${services.presence.url}", path="${services.presence.path}")
+@FeignClient(
+        name="${services.presence.name}",
+        url="${services.presence.url}",
+        path="${services.presence.path}",
+        fallback = PresenceFeign.Fallback.class)
 public interface PresenceFeign {
+
     @GetMapping
     List<UserPresenceDTO> getByUserIds(@RequestBody List<Long> userIds);
+
+
+    @Component
+    class Fallback implements PresenceFeign {
+
+        @Override
+        public List<UserPresenceDTO> getByUserIds(List<Long> userIds) {
+            return Collections.emptyList();
+        }
+    }
 }
--- Chat/src/main/java/com/cesar/Chat/feign/UserFeign.java
+++ Chat/src/main/java/com/cesar/Chat/feign/UserFeign.java
@@ -2,12 +2,27 @@ package com.cesar.Chat.feign;
 
 import com.cesar.Chat.dto.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import java.util.Collections;
 import java.util.List;
 
-@FeignClient(name="${services.user.name}", url="${services.user.url}", path="${services.user.path}")
+@FeignClient(name = "${services.user.name}",
+        url = "${services.user.url}",
+        path = "${services.user.path}",
+        fallback = UserFeign.UserFeignFallback.class)
 public interface UserFeign {
+
     @GetMapping
     List<UserDTO> getDetails(@RequestBody List<Long> ids);
+
+    @Component
+    static class UserFeignFallback implements UserFeign {
+
+        @Override
+        public List<UserDTO> getDetails(List<Long> ids) {
+            return Collections.emptyList();
+        }
+    }
 }
