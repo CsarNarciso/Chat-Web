ffd7d9fe84c008467598f581319184edb43aa435
CsarNarciso <cesarpazol1029@gmail.com>
2024-11-02 23:02:27 -0600

    User circuit breaker integration, on default profile don't connect to eureka nor configServer, and use H2 DB instead Postgres

--- User/src/main/java/com/cesar/User/feign/MediaFeign.java
+++ User/src/main/java/com/cesar/User/feign/MediaFeign.java
@@ -2,14 +2,32 @@ package com.cesar.User.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name="${services.media.name}", path="${services.media.path}")
+@FeignClient(name = "${services.media.name}", 
+			url = "${services.media.url}", 
+			path = "${services.media.path}",
+			fallback = MediaFeign.MediaFeignFallback.class)
 public interface MediaFeign {
-    @PostMapping
+    
+	@PostMapping
     String upload(@RequestParam MultipartFile imageMetadata, @RequestParam String oldPath);
-    @DeleteMapping
+    
+	@DeleteMapping
     void delete(@RequestParam String path);
+	
+	
+	
+	@Component
+    static class MediaFeignFallback implements MediaFeign {
+
+        @Override
+		String upload(MultipartFile imageMetadata, String oldPath){}
+    
+		@Override
+		void delete(String path){}
+    }
 }
