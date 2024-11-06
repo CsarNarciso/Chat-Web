983cf69b496ebc0096d22b2301a5e8c20697522c
CsarNarciso <cesarpazol1029@gmail.com>
2024-11-05 21:16:58 -0600

    Chat add lombok all/no argsconstructor annotation for mapping

--- Chat/src/main/java/com/cesar/Chat/dto/ConversationDTO.java
+++ Chat/src/main/java/com/cesar/Chat/dto/ConversationDTO.java
@@ -4,9 +4,13 @@ import lombok.Builder;
 import lombok.Data;
 import java.time.LocalDateTime;
 import java.util.UUID;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ConversationDTO {
     private UUID id;
     private LocalDateTime createdAt;
