0d024eb7b8a3a1caa11088a96a82895026a9309b
CsarNarciso <cesarpazol1029@gmail.com>
2024-11-12 16:53:44 -0600

    Chat serializable for message dto instead entity

--- Chat/src/main/java/com/cesar/Chat/dto/MessageDTO.java
+++ Chat/src/main/java/com/cesar/Chat/dto/MessageDTO.java
@@ -1,15 +1,16 @@
 package com.cesar.Chat.dto;
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class MessageDTO {
+public class MessageDTO implements Serializable {
     private UUID id;
     private UUID conversationId;
     private Long senderId;
--- Chat/src/main/java/com/cesar/Chat/entity/Message.java
+++ Chat/src/main/java/com/cesar/Chat/entity/Message.java
@@ -1,15 +1,19 @@
 package com.cesar.Chat.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
 
 @Data
 @Entity
 @Table(name="messages")
-public class Message implements Serializable {
+public class Message {
     @Id
     private UUID id;
     @Column(name="conversation_id")
