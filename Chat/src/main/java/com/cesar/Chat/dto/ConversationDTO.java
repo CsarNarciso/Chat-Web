52da35a84ace7725118cd2ef7293756627d0ea4d
CsarNarciso <cesarpazol1029@gmail.com>
2024-11-12 16:52:57 -0600

    Chat lombok all and no args constructor annotations for redis dtos

--- Chat/src/main/java/com/cesar/Chat/dto/ConversationDTO.java
+++ Chat/src/main/java/com/cesar/Chat/dto/ConversationDTO.java
@@ -4,11 +4,15 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ConversationDTO implements Serializable {
     private UUID id;
     private List<Long> recreateFor;
--- Chat/src/main/java/com/cesar/Chat/dto/ParticipantDTO.java
+++ Chat/src/main/java/com/cesar/Chat/dto/ParticipantDTO.java
@@ -1,11 +1,15 @@
 package com.cesar.Chat.dto;
 
 import java.io.Serializable;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ParticipantDTO implements Serializable {
     private Long userId;
 }
