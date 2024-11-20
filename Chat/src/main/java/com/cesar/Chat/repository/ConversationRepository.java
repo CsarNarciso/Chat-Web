de23b2be13aa12682e98dfa75fd74b095ffa5007
CsarNarciso <cesarpazol1029@gmail.com>
2024-11-20 09:03:47 -0600

    Chat user cant load deleted conversations

--- Chat/src/main/java/com/cesar/Chat/repository/ConversationRepository.java
+++ Chat/src/main/java/com/cesar/Chat/repository/ConversationRepository.java
@@ -2,10 +2,12 @@ package com.cesar.Chat.repository;
 
 import java.util.List;
 import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import com.cesar.Chat.entity.Conversation;
 
 @Repository
@@ -14,12 +16,13 @@ public interface ConversationRepository extends JpaRepository<Conversation, UUID
 	@Query(value="""
 			SELECT c.* FROM conversations c 
 			JOIN conversations_participants cp ON c.id = cp.conversation_id
-			WHERE cp.user_id IN :userIds
+			WHERE cp.user_id IN :userIds 
+			AND cp.user_id NOT MEMBER OF c.recreateFor
 			GROUP BY c.id 
 			HAVING COUNT(DISTINCT cp.user_id) = :userCount
 			""", nativeQuery=true)
 	Conversation findByUserIds(@Param("userIds") List<Long> userIds, @Param("userCount") int userCount);
 
-    @Query("SELECT c FROM Conversation c WHERE :userId MEMBER OF c.participants")
+    @Query("SELECT c FROM Conversation c WHERE :userId MEMBER OF c.participants AND :userId NOT MEMBER OF c.recreateFor")
     List<Conversation> findByUserId(@Param("userId") Long userId);
 }
