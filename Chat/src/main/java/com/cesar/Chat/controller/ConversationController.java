9a36b64100ce756a9e380ffa7f6bb040bcf17878
CsarNarciso <cesarpazol1029@gmail.com>
2024-11-22 13:02:29 -0600

    Chat notfound if empty list on controller loadmessages

--- Chat/src/main/java/com/cesar/Chat/controller/ConversationController.java
+++ Chat/src/main/java/com/cesar/Chat/controller/ConversationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cesar.Chat.dto.ConversationViewDTO;
@@ -23,6 +24,7 @@ import com.cesar.Chat.service.ConversationService;
 public class ConversationController {
 	
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
     public void onFirstInteraction(@RequestBody MessageForInitDTO initMessage){
         service.create(null, initMessage);
     }
--- Chat/src/main/java/com/cesar/Chat/controller/MessageController.java
+++ Chat/src/main/java/com/cesar/Chat/controller/MessageController.java
@@ -1,27 +1,42 @@
 package com.cesar.Chat.controller;
 
-import com.cesar.Chat.dto.MessageForSendDTO;
-import com.cesar.Chat.service.MessageService;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cesar.Chat.dto.MessageDTO;
+import com.cesar.Chat.dto.MessageForSendDTO;
+import com.cesar.Chat.service.MessageService;
 
 @RestController
 @RequestMapping("/messages")
 public class MessageController {
 
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
     public void onSend(@RequestBody MessageForSendDTO message){
         service.send(message);
     }
 
     @GetMapping(value = "/{conversationId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> onLoad(@PathVariable UUID conversationId) {
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(service.loadConversationMessages(conversationId));
+        List<MessageDTO> messages = service.loadConversationMessages(conversationId);
+    	return !messages.isEmpty() 
+    			? ResponseEntity
+                	.status(HttpStatus.OK)
+                	.body(messages)
+                : ResponseEntity.notFound().build();
     }
 
     @PutMapping(value = "/clean.unread/{conversationId}/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
