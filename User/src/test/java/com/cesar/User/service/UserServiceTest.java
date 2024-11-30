ae331a669beba6b0ff506558ec499216b31d95ac
CsarNarciso <cesarpazol1029@gmail.com>
2024-11-30 17:21:39 -0600

    User service test getbyids

--- User/src/test/java/com/cesar/User/service/UserServiceTest.java
+++ User/src/test/java/com/cesar/User/service/UserServiceTest.java
@@ -1,7 +1,16 @@
 package com.cesar.User.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -84,6 +93,41 @@ public class UserServiceTest {
 		assertEquals(USERNAME, userResult.getUsername());
 		assertEquals(EMAIL, userResult.getEmail());
 		assertEquals(IMAGE_URL, userResult.getProfileImageUrl());
+	}
+	
+	@Test
+	public void givenUserIds_whenGetByIds_thenReturnsListOfUserDTOs() {
+		
+		//Given
+		List<Long> ids = List.of(1l, 2l, 3l);
+		
+		//When
+		when(dataService.getByIds( ids )).thenReturn(List.of(
+						new UserDTO(1l, USERNAME, EMAIL, IMAGE_URL),
+						new UserDTO(2l, USERNAME, EMAIL, IMAGE_URL),
+						new UserDTO(3l, USERNAME, EMAIL, IMAGE_URL)));
+		List<UserDTO> usersResult = service.getByIds(ids);
+		
+		//Then
+		
+		//Verify Data Service interaction and capture User entity
+		verify(dataService, times(1)).getByIds(ids);
+		
+		//Verify NO ModelMapper interaction
+		verify(mapper, times(0)).map(any(), any());
+		
+		//Asserts on result
+		assertNotNull(usersResult);
+		assertFalse(usersResult.isEmpty());
+		assertEquals(ids.size(), usersResult.size());
+		
+		for(int i=0; i<usersResult.size(); i++) {
+			
+			assertEquals(ids.get(i), usersResult.get(i).getId());
+			assertEquals(USERNAME, usersResult.get(i).getUsername());
+			assertEquals(EMAIL, usersResult.get(i).getEmail());
+			assertEquals(IMAGE_URL, usersResult.get(i).getProfileImageUrl());
+		}
 	}
 	
 	
