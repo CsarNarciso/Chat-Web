686110bd6972ce7ef0226b244b5ff8b72298dc8e
CsarNarciso <cesarpazol1029@gmail.com>
2024-12-03 14:44:26 -0600

    User media service complete test coverage

--- User/src/test/java/com/cesar/User/service/MediaServiceTest.java
+++ User/src/test/java/com/cesar/User/service/MediaServiceTest.java
@@ -1,16 +1,7 @@
 package com.cesar.User.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
