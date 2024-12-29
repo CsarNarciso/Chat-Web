e643b1657fca66afdba8ed0606d4cbbfd367bc88
CsarNarciso <cesarpazol1029@gmail.com>
2024-12-29 17:20:35 -0600

    User init test: use resttemplate instead mockmvc for higher level of integration

--- User/src/test/java/com/cesar/User/controller/ControllerIntTest.java
+++ User/src/test/java/com/cesar/User/controller/ControllerIntTest.java
@@ -1,44 +1,49 @@
 package com.cesar.User.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
-import org.hamcrest.text.MatchesPattern;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import com.cesar.User.dto.UserDTO;
+
 @AutoConfigureMockMvc
 public class ControllerIntTest {
 
-    @Autowired
-    private MockMvc mvc;
-
     @LocalServerPort
     private int port;
+    
+    String serviceUrl = String.format("http://localhost:%s/users", port);
 
     @Value("${defaultImage.name}")
     private String defaultProfileImageName;
 
+    private final Long ID = 1L;
     private final String USERNAME = "USERNAME";
     private final String EMAIL = "EMAIL";
     private final String PASSWORD = "PASSWORD";
 
     @Test
     public void givenUserId_whenGetById_thenReturnUserDTO() throws Exception {
-        mvc.perform(get("/users/{id}", 1L))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.id").value(1L))
-            .andExpect(jsonPath("$.username").value(USERNAME))
-            .andExpect(jsonPath("$.email").value(EMAIL))
-            .andExpect(jsonPath("$.profileImageUrl").value(getDefaultImageUrl()));
+        
+    	RestTemplate restTemplate = new RestTemplate();
+    	
+    	String url = serviceUrl + "/{id}";
+    	
+    	ResponseEntity<UserDTO> response = restTemplate.exchange(url, HttpMethod.GET, null, UserDTO.class, ID);
+    	
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(USERNAME, response.getBody().getUsername());
+        assertEquals(EMAIL, response.getBody().getEmail());
     }
 
     public String getDefaultImageUrl() {
