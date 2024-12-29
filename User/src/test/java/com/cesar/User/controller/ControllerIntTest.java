006108fbe8a6f580d121de514ffefcbd52052e9c
CsarNarciso <cesarpazol1029@gmail.com>
2024-12-29 17:49:56 -0600

    User init test: create with media service integration by wiremock stub server

--- User/src/test/java/com/cesar/User/controller/ControllerIntTest.java
+++ User/src/test/java/com/cesar/User/controller/ControllerIntTest.java
@@ -2,25 +2,29 @@ package com.cesar.User.controller;
 
 
 import static org.junit.jupiter.api.Assertions.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-
+import org.springframework.web.multipart.MultipartFile;
+import com.cesar.User.dto.CreateRequestDTO;
 import com.cesar.User.dto.UserDTO;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.maciejwalkowiak.wiremock.spring.EnableWireMock;
 
-@AutoConfigureMockMvc
+@EnableWireMock
 public class ControllerIntTest {
 
     @LocalServerPort
     private int port;
     
-    String serviceUrl = String.format("http://localhost:%s/users", port);
+    String baseServiceUrl = String.format("http://localhost:%s/users", port);
 
     @Value("${defaultImage.name}")
     private String defaultProfileImageName;
@@ -29,13 +33,41 @@ public class ControllerIntTest {
     private final String USERNAME = "USERNAME";
     private final String EMAIL = "EMAIL";
     private final String PASSWORD = "PASSWORD";
-
+    private final String CUSTOM_IMAGE_URL = "http://localhost:8003/media/random-image-name.extension"; 
+    private final MultipartFile IMAGE_FILE = mock(MultipartFile.class);
+    
+    private final RestTemplate restTemplate = new RestTemplate();
+    
+    
+    
     @Test
-    public void givenUserId_whenGetById_thenReturnUserDTO() throws Exception {
+    public void givenFullCreateRequest_whenCreate_thenSaveNewUserInDBAndReturnAsUserDTO() throws Exception {
         
-    	RestTemplate restTemplate = new RestTemplate();
+    	WireMockServer wireMockServer = new WireMockServer();
+        wireMockServer.start();
     	
-    	String url = serviceUrl + "/{id}";
+        stubFor(post(urlEqualTo("http://localhost:8003/media/upload"))
+    				.withRequestBody(isNow())
+    			.willReturn(ok("http://localhost:8003/media/random-image-name.extension")));
+    	
+    	wireMockServer.stop();
+    	
+    	CreateRequestDTO createRequest = new CreateRequestDTO(USERNAME, EMAIL, PASSWORD, IMAGE_FILE);
+    	
+    	ResponseEntity<UserDTO> response = restTemplate.postForEntity(baseServiceUrl, createRequest, UserDTO.class);
+    	
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(USERNAME, response.getBody().getUsername());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(CUSTOM_IMAGE_URL, response.getBody().getProfileImageUrl());
+    }
+    
+    @Test
+    public void givenUserId_whenGetById_thenReturnUserDTO() {
+        
+    	String url = baseServiceUrl + "/{id}";
     	
     	ResponseEntity<UserDTO> response = restTemplate.exchange(url, HttpMethod.GET, null, UserDTO.class, ID);
     	
