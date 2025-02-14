3830a304072f1998dbe9df616b6ff06fb79698f3
CsarNarciso <cesarpazol1029@gmail.com>
2025-02-14 09:05:36 -0600

    Gateway remove discoveryrouterconfiguration

--- Gateway/src/main/java/com/cesar/Gateway/config/RouterConfiguration.java
+++ Gateway/src/main/java/com/cesar/Gateway/config/RouterConfiguration.java
@@ -7,8 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
-public class DirectRouterConfiguration {
+public class RouterConfiguration {
 
    
     @Bean
