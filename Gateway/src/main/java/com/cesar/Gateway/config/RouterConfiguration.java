b46a2464838dbf7eaf2bc3d97529d4c041d3f648
CsarNarciso <cesarpazol1029@gmail.com>
2025-02-17 11:00:10 -0600

    Add steps file to track development steps documentation

---
+++ Gateway/src/main/java/com/cesar/Gateway/config/RouterConfiguration.java
@@ -0,0 +1,36 @@
+package com.cesar.Gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class RouterConfiguration {
+
+   
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // Forward API requests for the user service
+                .route(r -> r
+                        .path("/users/**")
+                        .uri("http://localhost:8001"))
+                .route(r -> r
+                        .path("/conversations/**", "/messages/**")
+                        .uri("http://localhost:8002"))
+                // Route API docs for the user service
+                .route("user-service-docs", r -> r
+                        .path("/user-service/docs/**")
+                        .filters(f -> f.rewritePath("/user-service/docs/(?<segment>.*)", "/docs/${segment}"))
+                        .uri("http://localhost:8001"))
+                // Route Swagger UI for the user service
+                .route("user-service-swagger-ui", r -> r
+                        .path("/swagger-ui.html")
+                        .and()
+                        .query("configUrl", "/user-service/docs/swagger-config")
+                        .uri("http://localhost:8001"))
+                .build();
+    }
+}
