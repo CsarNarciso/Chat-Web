6f15124a339c0a8dddddd6ef5f505774cb717b9d
CsarNarciso <cesarpazol1029@gmail.com>
2025-03-19 14:28:48 -0600

    Media track mediatypenotsupported exception in controller advice

--- Media/src/main/java/com/cesar/Media/controller/ControllerAdvice.java
+++ Media/src/main/java/com/cesar/Media/controller/ControllerAdvice.java
@@ -1,12 +1,19 @@
 package com.cesar.Media.controller;
 
 import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdvice {
+
+	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+	public ResponseEntity<?> mediaTypeNotSupported(HttpMediaTypeNotSupportedException ex){
+		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(ex.getMessage());
+	}
 
 	@ExceptionHandler(RuntimeException.class)
 	public ResponseEntity<?> internalServer(RuntimeException ex){
